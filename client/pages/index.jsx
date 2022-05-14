389a720c34ac79b3702bf35960c48825983d77e2
Alihaiderizvi <alihaiderizvi.you@gmail.com>
2022-05-14 15:34:53 +0500

    Header

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,4 +1,3 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from "../components/Header";
@@ -7,7 +6,7 @@ const style = {
   wrapper: `h-screen max-h-screen h-min-screen w-screen bg-[#2D242F] text-white select-none flex flex-col justify-between`,
 }
 
-const Home: NextPage = () => {
+const Home = () => {
   return (
     <div className={style.wrapper}>
       <Header />
